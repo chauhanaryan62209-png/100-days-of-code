b6d824247fcd164f4014cdbc095677c3882b33f6
chauhanaryan62209-png <chauhanaryan62209@gmail.com>
2025-10-29 20:19:03 +0530

    Update day25.2.c
    .

---
+++ day25.2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+    int n = 5; 
+
+    for(i = 1; i <= n; i++) {
+        
+        for(space = 1; space < i; space++) {
+            printf(" ");
+        }
+
+        
+        for(j = i; j <= n; j++) {
+            printf("*");
+        }
+
+        printf("\n"); 
+    }
+
+    return 0;
+}
