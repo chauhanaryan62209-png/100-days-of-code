58d78ce15381ecafa0628752b88cc2063ee05602
chauhanaryan62209-png <chauhanaryan62209@gmail.com>
2025-11-02 19:51:11 +0530

    .

---
+++ day35.2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter number of positions to rotate (k): ");
+    scanf("%d", &k);
+
+    
+    k = k % n;
+
+    
+    int rotated[n];
+
+    
+    for (int i = 0; i < n; i++) {
+        rotated[(i + k) % n] = arr[i];
+    }
+
+    
+    printf("Array after rotating right by %d positions:\n", k);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", rotated[i]);
+    }
+
+    return 0;
+}
